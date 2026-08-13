05a5b4eff874b8c542f162daf18d9a4ba99ba9bd
Logan Anauo <logan.anauo@pitt.edu>
2026-08-12 21:59:54 -0400

    Update general_simulation.py

--- general_simulation.py
+++ general_simulation.py
@@ -2,62 +2,52 @@
 import random
 import numpy as np
 
+### parameters ###
 
-#-----------------------#
-# Simulation Parameters #
-#-----------------------#
-
-# years to simulate
+### years into the future
 Years = 50
 
-# number of runs
+### set number of runs
 simulations = 100_000
 
-# units of glacial ice mass (arbitrary)
+### units of glacial ice mass (arbitrary)
 initial_mass = 1000.00
 
-# climate assumptions
-mean_temp = 1.0 # deg C
+### climate assumptions; using degrees Celsius
+mean_temp = 1.0
 temp_std = 1.5
 
-mean_snow = 120 # cm/year
+### snowfall accumulation; cm/year
+mean_snow = 120
 snow_std = 20
 
-
-
-### each run, mass is recalculated and stored in this list
 final_masses = []
 
 
-# -----------------------#
-# Monte Carlo Simulation #
-# -----------------------#
+### monte carlo simulation logic ###
+final_masses = []
 
 for i in range(simulations):
-    
     mass = initial_mass
-    
     for year in range(Years):
         
-        
-        # random yearly climate
+        ### random yearly climate
         temperature = random.gauss(mean_temp, temp_std)
         snowfall= random.gauss(mean_snow, snow_std)
          
-        # snow accumulation
+        ### snow accumulation
         accumulation = snowfall * 0.50
          
-        # ice melt
+        ### ice melt
         if temperature > 0:
             melt = temperature * 40
         else:
-            
             melt = 0
          
-        # annual mass balance
+        ### annual mass balance
         mass += accumulation - melt
         
-        # glacier cannot have negative mass
+        ### glacier cannot have negative mass
         if mass < 0:
             mass = 0
             break
@@ -65,10 +55,7 @@ for i in range(simulations):
     final_masses.append(mass)
     
 
-#---------------------#
-#       Results       #
-#---------------------#
-
+### results ###
 average_mass = np.mean(final_masses)
 minimum_mass = min(final_masses)
 maximum_mass = max(final_masses)
